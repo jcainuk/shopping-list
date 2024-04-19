935d7c7b1115cd38edf062b4215b72d2ce139935
Jonathan Cain <jcainuk@gmail.com>
2024-04-19 15:27:21 +0100

    creating element and text node inside the addItem function

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const itemList = document.getElementById("item-list");
 const addItem = (e) => {
   e.preventDefault();
 
+  const newItem = itemInput.value;
+
   // Validate Input
-  if (itemInput.value === "") {
+  if (newItem === "") {
     alert("Please add an item");
     return;
   }
 
-  console.log("success");
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  console.log(li);
 };
 
 // Event Listeners
