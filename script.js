dd15391535dbd9e4d92c328edd7a1c17a2202c1a
Jonathan Cain <jcainuk@gmail.com>
2024-04-19 15:23:38 +0100

    start addItem function and create event listener on the form

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+
+const addItem = (e) => {
+  e.preventDefault();
+
+  // Validate Input
+  if (itemInput.value === "") {
+    alert("Please add an item");
+    return;
+  }
+
+  console.log("success");
+};
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
